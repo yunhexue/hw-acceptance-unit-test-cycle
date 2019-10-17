d1e3153549d2bfeb10c7ca5999b36c39b61548e8
EC2 Default User <ec2-user@ip-172-31-24-60.us-east-2.compute.internal>
2019-10-17 05:08:57 +0000

    BDD-TDD-test

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,7 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
-  
+  # root :to => redirect('/movies')
+  root 'movies#index'
   match '/movies/:id/similar(.:format)', to: 'movies#similar_direc', as: 'movie_similar', via: :get
 end
